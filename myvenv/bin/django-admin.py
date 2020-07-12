bc18b216787b9c9af1d949351189921e9573b96d
razor3538 <razor3538@mail.ru>
2020-07-12 17:40:50 +0300

    clear repo

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/razor3538/Desktop/Diferent-Python-projects/WEB/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
